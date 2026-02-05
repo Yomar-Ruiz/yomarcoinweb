9f0af5eaa3ac7ac87270fc20f32d4ac0097a676e
Yomar G Ruiz <yomargabrielruiz@gmail.com>
2026-02-05 16:50:55 -0400

    Delete programa junior directory

--- programa junior/index.js
+++
@@ -1,9 +0,0 @@
-let circulo = document.querySelector('.circulo')
-let color = document.querySelector('.color')
-let boton = document.querySelector('.Cambio')
-
-
-boton.addEventListener('click', (e) =>{
-    e.preventDefault();
-    circulo.style.backgroundColor = color.value;
-},)
